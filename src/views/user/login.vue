236c5e05b273552f41176d35319bafd7d659a1ae
Emil Zhai <root@derzh.com>
2017-09-04 12:22:27 +0800

    Change login password input type.

--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -5,7 +5,7 @@
         <el-input v-model="form.account"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="form.password"></el-input>
+        <el-input type="password" v-model="form.password"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit" :disabled="!form.account || !form.password">登录</el-button>
