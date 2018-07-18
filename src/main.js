0e0003c2f0e5f634931b7496432adbe40b39ac98
Emil Zhai <root@derzh.com>
2018-07-18 10:14:09 +0800

    Fake all web requests' referer.

--- src/main.js
+++ src/main.js
@@ -24,3 +24,20 @@ new Vue({
   store,
   render: h => h(App),
 });
+
+// Fake all web requests' referer.
+if (window.chrome && window.chrome.webRequest && window.chrome.webRequest.onBeforeSendHeaders) {
+  window.chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
+    const headers = {};
+    if (details.type === 'xmlhttprequest') {
+      const referer = details.requestHeaders.find(h => h.name === 'Referer');
+      if (referer) {
+        referer.value = details.url;
+      } else {
+        details.requestHeaders.push({ name: 'Referer', value: details.url });
+      }
+      headers.requestHeaders = details.requestHeaders;
+    }
+    return headers;
+  }, { urls: ['http://*/*', 'https://*/*'] }, ['requestHeaders', 'blocking']);
+}
