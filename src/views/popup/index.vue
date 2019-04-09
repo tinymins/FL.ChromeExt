73f08ea7f3c5f409bca071d5c7dcd659eaf55321
Emil Zhai <root@derzh.com>
2019-04-09 18:05:52 +0800

    feat: optimize popup page initial time

--- src/views/popup/index.vue
+++ src/views/popup/index.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="menu">
-    <el-button type="primary" class="menu-item" @click="popupCsort">超级排序</el-button>
-    <el-button type="primary" class="menu-item" @click="popupTsellTops">实时榜单</el-button>
-    <el-button type="primary" class="menu-item" @click="popupTsellCategory">分类榜单</el-button>
+    <template v-if="mounted">
+      <el-button type="primary" class="menu-item" @click="popupCsort">超级排序</el-button>
+      <el-button type="primary" class="menu-item" @click="popupTsellTops">实时榜单</el-button>
+      <el-button type="primary" class="menu-item" @click="popupTsellCategory">分类榜单</el-button>
+    </template>
   </div>
 </template>
 <script>
-import { popupWindow } from '@/utils/chrome-ext';
 import { Button } from 'element-ui';
+import { popupWindow } from '@/utils/chrome-ext';
 
 export default {
   components: {
     [Button.name]: Button,
+  },
+  data() {
+    return {
+      mounted: false,
+    };
+  },
+  mounted() {
+    window.addEventListener('resize', this.startRender);
+    this.timerRender = window.setTimeout(this.startRender, 50);
   },
   methods: {
     popupCsort() {
@@ -22,6 +33,14 @@ export default {
     },
     popupTsellCategory() {
       popupWindow('index.html#/tsell/category', true);
+    },
+    startRender() {
+      if (this.timerRender) {
+        clearTimeout(this.timerRender);
+        delete this.timerRender;
+      }
+      this.mounted = true;
+      window.removeEventListener('resize', this.onMounted);
     },
   },
 };
