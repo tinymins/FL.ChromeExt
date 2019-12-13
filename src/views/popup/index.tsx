27c78c59b70f44a8a5b9102adc2ab8aa4e0b508b
Emil Zhai <root@derzh.com>
2019-12-13 11:25:25 +0800

    refactor: popup item render logic

--- src/views/popup/index.tsx
+++ src/views/popup/index.tsx
@@ -14,21 +14,19 @@ import styles from '@/styles/views/popup/index.module.scss';
 
 Vue.use(Button);
 
+const POPUP_LIST = [
+  { path: 'csort', text: '超级排序' },
+  { path: 'tsell/realtime', text: '实时榜单' },
+  { path: 'tsell/category', text: '分类榜单' },
+];
+
 @Component
 export default class PopupPage extends Vue {
   private ready = false;
   private timerRender = 0;
 
-  private popupCsort(): void {
-    popupWindow('index.html#/csort', true);
-  }
-
-  private popupTsellRealtime(): void {
-    popupWindow('index.html#/tsell/realtime', true);
-  }
-
-  private popupTsellCategory(): void {
-    popupWindow('index.html#/tsell/category', true);
+  private popup(path): void {
+    popupWindow(`index.html#/${path}`, true);
   }
 
   private startRender(): void {
@@ -49,11 +47,9 @@ export default class PopupPage extends Vue {
     return <div class={styles.menu}>
       {
         this.ready
-          ? [
-            <el-button type="primary" class={styles['menu-item']} onClick={this.popupCsort}>超级排序</el-button>,
-            <el-button type="primary" class={styles['menu-item']} onClick={this.popupTsellRealtime}>实时榜单</el-button>,
-            <el-button type="primary" class={styles['menu-item']} onClick={this.popupTsellCategory}>分类榜单</el-button>,
-          ]
+          ? POPUP_LIST.map(item => <div class={styles['menu-item']}>
+            <el-button class={styles['menu-item__btn']} type="primary" onClick={() => this.popup(item.path)}>{ item.text }</el-button>
+          </div>)
           : null
       }
     </div>;
